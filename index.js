8c2e704388542009decba05528eede04891c96a4
ajay <ajay.kumar@unicodesystems.org>
2018-07-02 12:11:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ module.exports = { name, height
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected the name to be Susan")
+    console.log("Expected the name to be " + Susan)
 }
 
 if (height === "74") {
     console.log("The name is correct")
 } else {
-    console.log("Expected the name to be Susan")
+    console.log("Expected the height to be" + height)
 }
